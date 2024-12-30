e282db9d58fe9343dba70c006f0858067280709a
Hiromi ISHII <konn.jinro@gmail.com>
2024-12-30 20:53:10 +0900

    fix: clipy happy

--- qualified_do/examples/debug.rs
+++ qualified_do/examples/debug.rs
@@ -1,14 +1,12 @@
 fn main() {
     use functo_rs::data::*;
-    use functo_rs::impls::*;
     use qualified_do::qdo;
     let is = vec![1, 2, 3];
     let js = vec![4, 5, 6];
-    let ans: Vec<i64> = qdo! {ZipVec {
+    let _: Vec<i64> = qdo! {ZipVec {
         i <- is.clone();
         j <- js.clone();
         let k = 100i64;
-        let l = 100i64;
         return i + j + k
     }};
 }

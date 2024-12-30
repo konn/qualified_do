605c7cbc846a68eea17b7cf64e0c9d9fbe2c4d25
Hiromi ISHII <konn.jinro@gmail.com>
2024-12-30 21:57:47 +0900

    fix

--- qualified_do/examples/debug.rs
+++ qualified_do/examples/debug.rs
@@ -2,10 +2,8 @@ fn main() {
     use functo_rs::data::*;
     use qualified_do::qdo;
 
-    qdo! {ZipVec {
+    let _ = qdo! {AsData::<ZipVec> {
         x <- vec![1,2,3];
-        y <- vec![4,5,6];
-        if x % 2 == 1 { vec![()]} else { vec![] };
-        return x + y;
-    }}
+        return x
+    }};
 }

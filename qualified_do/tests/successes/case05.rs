c465184e0f587ef2ee03079329e571a6759d5ce5
Hiromi ISHII <konn.jinro@gmail.com>
2024-12-30 21:59:42 +0900

    ZipVec

---
+++ qualified_do/tests/successes/case05.rs
@@ -0,0 +1,20 @@
+fn main() {
+    use functo_rs::data::*;
+    use qualified_do::qdo;
+    let is = vec![1, 2, 3];
+    let js = vec![4, 5, 6];
+
+    let ans: Vec<i64> = qdo! {ZipVec {
+        i <- is.clone();
+        j <- js.clone();
+        let k = 100i64;
+        return i + j + k
+    }};
+    assert_eq!(
+        ans,
+        is.into_iter()
+            .zip(js)
+            .map(|(i, j)| i + j + 100)
+            .collect::<Vec<_>>()
+    );
+}

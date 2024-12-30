e51eef83c73c052d1c4dfb1f00a4ec44292468ad
Hiromi ISHII <konn.jinro@gmail.com>
2024-12-30 22:14:31 +0900

    ZipVec

--- qualified_do/tests/trybuild.rs
+++ qualified_do/tests/trybuild.rs
@@ -1,28 +1,32 @@
 use std::path::Path;
 
+fn list_rs<P: AsRef<Path>>(p: P) -> Vec<std::fs::DirEntry> {
+    let mut entries = std::fs::read_dir(p)
+        .unwrap()
+        .filter_map(|a| {
+            a.ok().and_then(|e| {
+                e.path()
+                    .extension()
+                    .and_then(|ext| if ext == "rs" { Some(e) } else { None })
+            })
+        })
+        .collect::<Vec<_>>();
+    entries.sort_by_key(|a| a.file_name());
+    entries
+}
+
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
     let tests_dir = Path::new("tests");
     let successes = tests_dir.join("successes");
-
-    let mut successes = successes
-        .read_dir()
-        .unwrap()
-        .collect::<Result<Vec<_>, _>>()
-        .unwrap();
-    successes.sort_by_key(|a| a.file_name());
+    let successes = list_rs(successes);
     for entry in successes {
         t.pass(entry.path());
     }
 
     let failures = tests_dir.join("failures");
-    let mut failures = failures
-        .read_dir()
-        .unwrap()
-        .collect::<Result<Vec<_>, _>>()
-        .unwrap();
-    failures.sort_by_key(|a| a.file_name());
+    let failures = list_rs(failures);
     for entry in failures {
         t.compile_fail(entry.path());
     }

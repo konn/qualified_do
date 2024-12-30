989b9ca9e2659f4a3fb5a4cb0d842d0fa2cdc4c2
Hiromi ISHII <konn.jinro@gmail.com>
2024-12-30 21:56:39 +0900

    Restructured tets

--- qualified_do/tests/trybuild.rs
+++ qualified_do/tests/trybuild.rs
@@ -1,11 +1,31 @@
+use std::path::Path;
+
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/case01.rs");
-    t.pass("tests/case02.rs");
-    t.pass("tests/case03.rs");
-    t.pass("tests/case04.rs");
-    t.pass("tests/case05.rs");
-    t.compile_fail("tests/error_case01.rs");
-    t.compile_fail("tests/error_case02.rs");
+    let tests_dir = Path::new("tests");
+    let successes = tests_dir.join("successes");
+
+    let mut successes = successes
+        .read_dir()
+        .unwrap()
+        .into_iter()
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    successes.sort_by_key(|a| a.file_name());
+    for entry in successes {
+        t.pass(entry.path());
+    }
+
+    let failures = tests_dir.join("failures");
+    let mut failures = failures
+        .read_dir()
+        .unwrap()
+        .into_iter()
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    failures.sort_by_key(|a| a.file_name());
+    for entry in failures {
+        t.compile_fail(entry.path());
+    }
 }

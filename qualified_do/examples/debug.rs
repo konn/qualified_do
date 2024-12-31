fca948774fe4f9abd512bdc8a852c53937061421
Hiromi ISHII <konn.jinro@gmail.com>
2024-12-31 16:19:18 +0900

    guards

--- qualified_do/examples/debug.rs
+++ qualified_do/examples/debug.rs
@@ -1,10 +1,11 @@
 fn main() {
-    use functo_rs::data::*;
+    use functo_rs::nonlinear::*;
     use qualified_do::qdo;
 
-    let _: Vec<i64> = qdo! {ZipVec {
+    let _: Vec<i64> = qdo! {UndetVec {
         x <- vec![1,2,3];
         y <- vec![4, 6];
+        guard 2 % 2 == 0;
         return x + y
     }};
 }

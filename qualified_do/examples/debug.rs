84502a0f8e1342e65b06be26a2b2a05da659a658
Hiromi ISHII <konn.jinro@gmail.com>
2024-12-31 16:20:57 +0900

    silence clippy

--- qualified_do/examples/debug.rs
+++ qualified_do/examples/debug.rs
@@ -1,11 +1,11 @@
 fn main() {
     use functo_rs::nonlinear::*;
     use qualified_do::qdo;
-
+    let j = 1 + 1;
     let _: Vec<i64> = qdo! {UndetVec {
         x <- vec![1,2,3];
         y <- vec![4, 6];
-        guard 2 % 2 == 0;
+        guard j % 2 == 0;
         return x + y
     }};
 }
